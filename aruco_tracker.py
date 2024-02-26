a06a8190fb760c55f8f916a58d37a93bb824f20a
travisbouchard <108905549+travisbouchard@users.noreply.github.com>
2024-02-26 14:13:26 -0600

    edits

--- aruco_tracker.py
+++ aruco_tracker.py
@@ -12,8 +12,39 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import glob
+import pyrealsense2 as rs
 
-cap = cv2.VideoCapture(0)
+marker_size = 5
+calib_size = 2.1
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+
+
+pipeline.start(config)
 
 ####---------------------- CALIBRATION ---------------------------
 # termination criteria for the iterative algorithm
@@ -21,23 +52,24 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 # checkerboard of size (7 x 6) is used
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = objp * calib_size
 # arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
 # iterating through all calibration images
 # in the folder
-images = glob.glob('calib_images/checkerboard/*.jpg')
-
+images = glob.glob('calib_images/checkerboard/*.png')
+count = 0
 for fname in images:
+    count += 1
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # find the chess board (calibration pattern) corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
 
     # if calibration pattern is found, add object points,
     # image points (after refining them)
@@ -49,14 +81,19 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-
+        img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
+        filename = str('images/calib_dots' + str(count) + str('.png'))
+        cv2.imwrite(filename, img)
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
 ###------------------ ARUCO TRACKER ---------------------------
 while (True):
-    ret, frame = cap.read()
+    frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
+
+    # Convert images to numpy arrays
+    frame = np.asanyarray(color_frame.get_data())
     #if ret returns false, there is likely a problem with the webcam/camera.
     #In that case uncomment the below line, which will replace the empty frame 
     #with a test image used in the opencv docs for aruco at https://www.docs.opencv.org/4.5.3/singlemarkersoriginal.jpg
@@ -66,7 +103,7 @@ while (True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # set dictionary size depending on the aruco marker selected
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
 
     # detector parameters can be set here (List of detection parameters[3])
     parameters = aruco.DetectorParameters_create()
@@ -84,12 +121,13 @@ while (True):
 
         # estimate pose of each marker and return the values
         # rvet and tvec-different from camera coefficients
-        rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
+        rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, marker_size, mtx, dist)
+        print(tvec)
         #(rvec-tvec).any() # get rid of that nasty numpy value array error
 
         for i in range(0, ids.size):
             # draw axis for the aruco markers
-            cv2.drawFrameAxes(frame, mtx, dist, rvec[i], tvec[i], 0.1)
+            cv2.drawFrameAxes(frame, mtx, dist, rvec[i], tvec[i], marker_size / 2)
 
         # draw a square around the markers
         aruco.drawDetectedMarkers(frame, corners)
@@ -113,7 +151,6 @@ while (True):
         break
 
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+pipeline.stop()
 
 
--- pose_estimation.py
+++ pose_estimation.py
@@ -6,21 +6,21 @@ import glob
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('calib_images/*.jpg')
+images = glob.glob('calib_images/checkerboard/*.png')
 
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -30,7 +30,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+        img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
 
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
@@ -40,6 +40,7 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.sh
 # Draw axis function can also be used.
 def draw(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
+    print(tuple(imgpts[0].ravel()))
     img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
     img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
     img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
@@ -47,13 +48,14 @@ def draw(img, corners, imgpts):
 
 axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
 
-for fname in glob.glob('calib_images/checkerboard/left03.jpg'):
+for fname in glob.glob('calib_images/checkerboard/calib6_Color.png'):
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
 
     if ret == True:
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        print(corners2)
 
         # Find the rotation and translation vectors.
         _,rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
